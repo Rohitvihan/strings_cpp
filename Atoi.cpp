f6ac5980ab7ed25f3940cc734a08a9bd94c2cda3
Rohit Vihan K <167102294+Rohitvihan@users.noreply.github.com>
2025-06-28 15:47:01 +0530

    Add files via upload

---
+++ Atoi.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+    int myAtoi(string s)
+    {
+        int start = 0;
+        int sign = 1;
+        if(s[0] == '-') {
+            sign = -1;
+            start = 1;
+        }
+        int ans = 0;
+        for(int i = start; i < s.size(); i++) {
+            if(s[i] >= '0' && s[i] <= '9') {
+                int d = s[i] - '0';
+                ans = ans * 10 + d;
+            }
+            else {
+                return -1;
+            }
+        }
+        return ans * sign;
+    }
+
+
+int main() {
+
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+
+    int result = myAtoi(input);
+    cout << "Converted integer: " << result << endl;
+
+    return 0;
+}
