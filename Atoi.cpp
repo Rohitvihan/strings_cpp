37ac952d30a010e79527e2872b41e08d7a9b9bb5
Rohit Vihan K <167102294+Rohitvihan@users.noreply.github.com>
2025-06-28 15:49:45 +0530

    Update Atoi.cpp

--- Atoi.cpp
+++ Atoi.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-
-    int myAtoi(string s)
-    {
-        int start = 0;
-        int sign = 1;
-        if(s[0] == '-') {
-            sign = -1;
-            start = 1;
-        }
-        int ans = 0;
-        for(int i = start; i < s.size(); i++) {
-            if(s[i] >= '0' && s[i] <= '9') {
-                int d = s[i] - '0';
-                ans = ans * 10 + d;
-            }
-            else {
-                return -1;
-            }
-        }
-        return ans * sign;
+int myAtoi(string s)
+{
+    int start = 0;
+    int sign = 1;
+     if(s[0] == '-') 
+     {
+        sign = -1;
+        start = 1;
     }
-
-
+    int ans = 0;
+    for(int i = start; i < s.size(); i++) 
+    {
+        if(s[i] >= '0' && s[i] <= '9') 
+        {
+            int d = s[i] - '0';
+            ans = ans * 10 + d;
+        }
+        else 
+        {
+            return -1;
+        }
+    }
+    return ans * sign;
+}
 int main() {
 
     string input;
